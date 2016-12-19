cc4394a1d4e7bff9995b23ac8252ad82db1e8a67
Leonard Buckley <lizard0king@gmail.com>
2016-12-18 22:04:26 -0800

    create github pages

--- assets/puritym/js/script.js
+++ assets/puritym/js/script.js
@@ -24,7 +24,7 @@ b);b()})})})(jQuery);
         var e = document.createElement("section");
         e.id = "top-1";
         e.className = "engine";
-        e.innerHTML = '<a href="https://mobirise.com">mobirise.com</a> Mobirise v3.10.2';
+        e.innerHTML = '<a href="https://mobirise.com">mobirise.com</a> Mobirise v3.10.3';
         document.body.insertBefore(e, document.body.childNodes[0]);
     }
 }();
